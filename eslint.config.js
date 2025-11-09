32fe6a1a45f64ae7ddfb016661d812c3d239e865
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-11-09 17:27:09 -0300

    🔧 update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,9 @@
-import js from '@eslint/js';
 import astro from 'eslint-plugin-astro';
 
 export default [
-  js.configs.recommended,
   {
-    files: ['**/*.astro'],
-    languageOptions: {
-      parser: astro.parsers.astro,
-    },
-    plugins: {
-      astro,
-    },
-    rules: {
-      ...astro.configs['flat/recommended'].rules,
-    },
+    ignores: ['node_modules/**'],
   },
+  ...astro.configs['flat/recommended'],
 ];
+
